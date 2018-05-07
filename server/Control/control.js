255c51d13b5018b6559c613eb1e3d9ebb02a9ad9
unknown <tranquanglinh.pt@gmai.com>
2018-05-07 15:04:33 +0700

    api get -> post

--- server/Control/control.js
+++ server/Control/control.js
@@ -51,9 +51,7 @@ module.exports= {
         app.get('/admin/getTerms', (req,res)=>admin_router.getTerms(req,res));
         // lấy thông tin các kì thực tập
 
-        app.get('admin/createTerm',(req,res)=> admin_router.createTerm(req,res)); // chưa viết xong phần router, model chưa rõ ràng
-        //thêm đợt thực tập
-
-        
+        app.post('admin/createTerm',jsonParser,(req,res)=> admin_router.createTerm(req,res)); // chưa viết xong phần router, model chưa rõ ràng
+        //thêm đợt thực tập        
     }
 }
