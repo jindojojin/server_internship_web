0619c85a05a97df65203184c72325aeb53671e89
unknown <tranquanglinh.pt@gmai.com>
2018-05-17 21:38:00 +0700

    thêm các hàm delete cơ bản

--- server/Model/DatabaseModel/database_delete.js
+++ server/Model/DatabaseModel/database_delete.js
@@ -1,5 +1,5 @@
-var Sequelize = require('sequelize')
-const Op = Sequelize.Op;
+// var Sequelize = require('sequelize')
+// const Op = Sequelize.Op;
 var model_required = require('./switchRequireModel') // lựa chọn bảng trong database phù hợp với kiểu người dùng;
 
 var database_delete = {
@@ -48,6 +48,151 @@ var database_delete = {
         }
     },
 
+    deleteAccount:async function(userID){
+        try {
+            let table = new model_required('account'); 
+            let result = await table.destroy({
+                where: {
+                  userID:userID
+                }
+              });
+            return Promise.resolve(result);
+        } catch (error) {
+            return Promise.reject(error)
+        }
+    },
+    deleteStudent:async function(studentID){
+        try {
+            let table = new model_required('student'); 
+            let result = await table.destroy({
+                where: {
+                  account_userID: studentID,
+                }
+              });
+            return Promise.resolve(result);
+        } catch (error) {
+            return Promise.reject(error)
+        }
+    },
+    deletePartner:async function(partnerID){
+        try {
+            let table = new model_required('partner'); 
+            let result = await table.destroy({
+                where: {
+                  account_userID:partnerID
+                }
+              });
+            return Promise.resolve(result);
+        } catch (error) {
+            return Promise.reject(error)
+        }
+    },
+    deleteLecturer:async function(lecturerID){
+        try {
+            let table = new model_required('lecturer'); 
+            let result = await table.destroy({
+                where: {
+                  account_userID:lecturerID
+                }
+              });
+            return Promise.resolve(result);
+        } catch (error) {
+            return Promise.reject(error)
+        }
+    },
+    deleteAdmin:async function(adminID){
+        try {
+            let table = new model_required('admin'); 
+            let result = await table.destroy({
+                where: {
+                  account_userID:adminID
+                }
+              });
+            return Promise.resolve(result);
+        } catch (error) {
+            return Promise.reject(error)
+        }
+    },
+    deleteComment:async function(commentID){
+        try {
+            let table = new model_required('comment'); 
+            let result = await table.destroy({
+                where: {
+                  commentID:commentID
+                }
+              });
+            return Promise.resolve(result);
+        } catch (error) {
+            return Promise.reject(error)
+        }
+    },
+    deleteFile:async function(fileID){
+        try {
+            let table = new model_required('file'); 
+            let result = await table.destroy({
+                where: {
+                  fileID:fileID
+                }
+              });
+            return Promise.resolve(result);
+        } catch (error) {
+            return Promise.reject(error)
+        }
+    },
+    deleteInternship_job:async function(jobID){
+        try {
+            let table = new model_required('internship_job'); 
+            let result = await table.destroy({
+                where: {
+                  jobID:jobID
+                }
+              });
+            return Promise.resolve(result);
+        } catch (error) {
+            return Promise.reject(error)
+        }
+    },
+    deletePlan_report:async function(plan_reportID){
+        try {
+            let table = new model_required('plan_report'); 
+            let result = await table.destroy({
+                where: {
+                  plan_reportID:plan_reportID
+                }
+              });
+            return Promise.resolve(result);
+        } catch (error) {
+            return Promise.reject(error)
+        }
+    },
+    deleteStudent_assession:async function(studentID,assessorID){
+        try {
+            let table = new model_required('student_assession'); 
+            let result = await table.destroy({
+                where: {
+                  studentID: studentID,
+                  assessorID:assessorID
+                }
+              });
+            return Promise.resolve(result);
+        } catch (error) {
+            return Promise.reject(error)
+        }
+    },
+    deleteStudent_skill:async function(skillID,studentID){
+        try {
+            let table = new model_required('student_skill'); 
+            let result = await table.destroy({
+                where: {
+                  studentID: studentID,
+                  skillID:skillID
+                }
+              });
+            return Promise.resolve(result);
+        } catch (error) {
+            return Promise.reject(error)
+        }
+    },  
     deleteTerm: async function(termID){
         try {
             let table = new model_required('term'); 
--- server/Model/DatabaseModel/database_update.js
+++ server/Model/DatabaseModel/database_update.js
@@ -76,27 +76,14 @@ var database_update = {
 
     },
 
-    update_account: async function (userID, new_Username, new_Nickname, new_Type) {
+    update_account: async function (userID, new_Infor) {
         try {
             let acc = model_require("account");
-            if (new_Type != null) { //kiểu người dùng bị thay đổi => xóa bỏ bản ghi từ kiểu người dùng cũ, thêm mới bản ghi vào phía người dùng mới
-                await acc.update({
-                    username: new_Username,
-                    nickname: new_Nickname,
-                    type: new_Type
-                }, {
-                        where: { userID: userID }
-                    });
-                
-            } else {
-                await acc.update({
-                    username: new_Username,
-                    nickname: new_Nickname
-                }, {
-                        where: { userID: userID }
-                    });
-            }
-
+            await acc.update({
+                new_Infor
+            }, {
+                    where: { userID: userID }
+                });
             return Promise.resolve(true);
         } catch (error) {
             return Promise.reject(false);
@@ -104,13 +91,14 @@ var database_update = {
 
     },
 
-    update_profile: async function(userType,userID,newProfile){
+    update_profile: async function (userType, userID, newProfile) {
         try {
             let profile = model_require(userType);
-            await profile.update(newProfile,{
-                where:{
-                account_userID:userID
-            }});
+            await profile.update(newProfile, {
+                where: {
+                    account_userID: userID
+                }
+            });
             return Promise.resolve(true);
         } catch (error) {
             return Promise.reject(error);
@@ -123,24 +111,24 @@ module.exports = database_update;
 // database_update.update_student_follow_X("1", "3", "job").then(r => console.log(r)).catch(e => console.log(e));
 
 let profile = {
-    class:"K61-CD",
-    grade:"2",
-    majors:"Công nghệ thông tin",
-    address:"Khu 5, Xuân Lộc, Thanh Thủy, Phú Thọ",
-    name:"Trần Quang Linh",
-    dateOfBirth:"19980501",
-    vnumail:"16021031",
+    class: "K61-CD",
+    grade: "2",
+    majors: "Công nghệ thông tin",
+    address: "Khu 5, Xuân Lộc, Thanh Thủy, Phú Thọ",
+    name: "Trần Quang Linh",
+    dateOfBirth: "19980501",
+    vnumail: "16021031",
     gpa: 34,
-    graduationYear:"2020",
-    email:"Tranquanglinh.pt@gmail.com",
-    skypeID:"",
-    facebook:"https://www.facebook.com/jindojojin",
-    phoneNumber:"16021031",
+    graduationYear: "2020",
+    email: "Tranquanglinh.pt@gmail.com",
+    skypeID: "",
+    facebook: "https://www.facebook.com/jindojojin",
+    phoneNumber: "16021031",
     position: "",
-    favorite:"Tốc độ, xem phim của Marvel",
-    orientation:"Công nghệ thông tin",
-    note:"Thích làm web",
-    picture:""
+    favorite: "Tốc độ, xem phim của Marvel",
+    orientation: "Công nghệ thông tin",
+    note: "Thích làm web",
+    picture: ""
 }
 
 // database_update.update_profile('student',1032,profile).then(r=> console.log("Thành công")).catch(e => console.log(e));
